d201c6040e3794a0b9edd9ad15b872fa10aa7c00
sampassarelli <passarelli.sam@gmail.com>
2020-12-09 21:23:40 -0600

    commit tree 8279610991dca88637ca0e91403749bf3908c1b8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let thankYouCards = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(num){
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num)
+}
